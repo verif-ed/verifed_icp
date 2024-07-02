f2fa29f20665befb262581d70dad0d742005fd30
lokesh-katari <lokeshkatari921@outlook.com>
2024-07-02 14:58:45 +0530

    changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -79,7 +79,7 @@ const Navbar = () => {
               </div>
               <ul
                 tabIndex={0}
-                className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+                className="menu menu-sm dropdown-content  mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
               >
                 <li>
                   <Link to="/dashboard">Dashboard</Link>
